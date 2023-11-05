826f93119180d0145ddf1fa2d8faef0df86bb1a3
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-05 23:46:44 +0100

    fetchFilm test

--- utils/fetchFilms.js
+++ utils/fetchFilms.js
@@ -1,1 +1,14 @@
+async function fetchFilm(title){
+    let response = await fetch(`http://www.omdbapi.com/?apikey=f7eee519&t=${title}`);
+    let data = await response.json();
+   
+    if(data.Response === 'True'){
+       return data
+    } else{
+        return null
+    }
 
+    
+}
+
+module.exports = fetchFilm
