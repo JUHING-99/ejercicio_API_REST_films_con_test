df9a027b8292fadb2963855fb952613b9162e31d
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-06 00:25:37 +0100

    some films tests pass

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const app = express()
 app.use(express.json())
 
 
-app.use('/films', films)
 
+app.use('/api', films)
 
+app.use((req, res, next) => {
+    next({ statusCode: 404, error: new Error('path not found') })
+})
+
+app.use(({ statusCode, error }, req, res, next) => {
+    res.status(statusCode).json({
+        success: false,
+        message: error.message,
+    })
+})
+
+module.exports = app
 app.listen(PORT, () => console.info(`> listening at http://localhost:${PORT}`))
--- routes/films.js
+++ routes/films.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
+const fetchFilm = require('../utils/fetchFilms')
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        data: {
-            message: 'response from GET "/films" endpoint',
-        }
-    })
+router.get('/film/:title', async(req, res, next) => {
+    let film = await fetchFilm(req.params.title)
+    const newObj = Object.fromEntries(
+        Object.entries(film).map(([k, v]) => [k.toLowerCase(), v])
+      );
+    console.log(newObj);
+    res.status(200).json(newObj)
 })
 
 
 module.exports = router
+
